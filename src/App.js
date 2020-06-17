986c12cf23e169af2cadd7a8e8551f216bc5d358
Katie Carroll <ktcarroll4@gmail.com>
2020-06-17 11:30:20 -0500

    HelloWorld working

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HelloWorld from "./state-drills/HelloWorld";
+
+function App() {
+  return <main className="App">{<HelloWorld />}</main>;
+}
+
+export default App;
